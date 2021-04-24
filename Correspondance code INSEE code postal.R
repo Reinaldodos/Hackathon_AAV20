0d1d01af3a7a44c565d671e0e35d0365898b6186
Reinaldo Dos Santos <reinalddos@gmail.com>
2021-04-24 10:22:35 +0200

    Correspondance codes INSEE codes postaux

---
+++ Correspondance code INSEE code postal.R
@@ -0,0 +1,46 @@
+Bases_urbaines %>% 
+  select(CODGEO, LIBGEO, geometry) %>% 
+  sf::st_transform(crs = 4326) %>% 
+  sf::st_centroid() 
+
+Correspondance_INSEE_Poste = 
+  "https://public.opendatasoft.com/explore/dataset/correspondance-code-insee-code-postal/download/?format=geojson&timezone=Europe/Berlin&lang=fr" %>% 
+  sf::st_read() %>% 
+  select(insee_com, postal_code, nom_comm, geometry)
+
+test = 
+  Correspondance_INSEE_Poste %>% as.data.frame() %>%
+  select(insee_com, postal_code, nom_comm)
+
+list(
+  Bases_urbaines %>% as.data.frame() %>%
+    select(CODGEO, LIBGEO) ,
+  test
+) %>%
+  reduce(.f = full_join, by = c("CODGEO" = "insee_com")) %>%
+  filter(is.na(postal_code))
+
+test = 
+  tribble(
+    ~insee_com, ~nom_comm, ~postal_code,
+    "55138", "CULEY", "55000",
+    "76095", "BIHOREL", "76420",
+    "76601", "SAINT-LUCIEN", "76780") %>% 
+  bind_rows(test)
+
+output = 
+  test %>% 
+  mutate_at(.vars = "nom_comm", .funs = str_to_title) %>% 
+  mutate(Recherche = str_c(nom_comm, " (", postal_code, ")")) %>% 
+  inner_join(x = Bases_urbaines %>% select(CODGEO, geometry), 
+             by = c("CODGEO" = "insee_com")) %>% 
+  sf::st_transform(crs = 4326) %>% 
+  sf::st_centroid() 
+
+sf::st_coordinates(output) %>% 
+  cbind.data.frame(output, .) %>% 
+  select(-geometry) %>% 
+  data.table::data.table() %>% 
+  rio::export(file = "data/recherche code postal.csv")
+  
+  
